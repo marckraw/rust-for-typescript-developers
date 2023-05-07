205825685d29a372c5f49266b93d8602a36d1efc
marckraw <marckraw@icloud.com>
2023-05-07 22:58:16 +0200

    some test codE

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+fn not_compelte_fn(x: usize) -> bool {
+    if x > 10 {
+        return true;
+    }
+
+    todo!("Hey, me finish this later");
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", not_compelte_fn(12));
+    println!("{}", not_compelte_fn(2));
+
+    let foo = Some(5); // Option - i32 | undefined
+    let bar = foo.unwrap(); // takes a value form foo (even if it can be undefined, dangerous)
 }
